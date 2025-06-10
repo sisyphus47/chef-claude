f4a94fce88be431830cec44f995ca7acc044b213
sarim19 <sarimansari313@gmail.com>
2025-06-11 02:50:20 +0530

    header

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import claudeimg from "../assets/chef-claude-icon.png";
+const Header = () => {
+  return (
+    <header>
+      <img src={claudeimg} alt="logo" />
+      <h1>Chef Claude</h1>
+    </header>
+  );
+};
+
+export default Header;
