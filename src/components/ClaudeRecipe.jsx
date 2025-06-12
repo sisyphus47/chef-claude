14f8bcebb5b4300044cb7c00490a3199c96c87af
sarim19 <sarimansari313@gmail.com>
2025-06-12 14:41:18 +0530

    final

--- src/components/ClaudeRecipe.jsx
+++ src/components/ClaudeRecipe.jsx
@@ -1,7 +1,10 @@
-export default function ClaudeRecipe() {
+import ReactMarkdown from "react-markdown";
+
+export default function ClaudeRecipe(props) {
   return (
-    <section>
+    <section className="suggested-recipe-container" aria-live="polite">
       <h2>Chef Claude Recommends:</h2>
+      <ReactMarkdown>{props.recipe}</ReactMarkdown>
     </section>
   );
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import IngredientsList from "./IngredientsList";
 import ClaudeRecipe from "./ClaudeRecipe";
+import { getRecipeFromMistral } from "./ai";
 
 export default function Main() {
   const [ingredients, setIngredients] = React.useState([]);
-  const [recipeShown, setRecipeShown] = React.useState(false);
+  const [recipe, setRecipe] = React.useState("");
 
-  function toggleRecipeShown() {
-    setRecipeShown((prevShown) => !prevShown);
+  async function getRecipe() {
+    const recipeMarkdown = await getRecipeFromMistral(ingredients);
+    setRecipe(recipeMarkdown);
   }
 
   function addIngredient(formData) {
@@ -26,15 +28,10 @@ export default function Main() {
         />
         <button>Add ingredient</button>
       </form>
-
       {ingredients.length > 0 && (
-        <IngredientsList
-          ingredients={ingredients}
-          toggleRecipeShown={toggleRecipeShown}
-        />
+        <IngredientsList ingredients={ingredients} getRecipe={getRecipe} />
       )}
-
-      {recipeShown && <ClaudeRecipe />}
+      {recipe && <ClaudeRecipe recipe={recipe} />}
     </main>
   );
 }
